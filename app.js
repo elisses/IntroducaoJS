9093222453d941205848d9a8ba61e8cc04a0c184
elisses <fernandalisses@gmail.com>
2019-05-15 17:03:21 +0200

    Correção

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-var input = document.getElementById("quantidade");
+    
+    var input = document.getElementById("quantidade");
+
+    var botaoIncrementa = document.querySelector("#btn-incrementa");
+    botaoIncrementa.addEventListener('click', incrementa);
+
+    var botaoDecrementa = document.querySelector("#btn-decrementa");
+    botaoDecrementa.addEventListener('click', decrementa);
+
 
 function incrementa()
 {
     input.value++;
+    var item = botaoIncrementa.closest('.item');
+    var preco = pegaPrecoItem(item);
+
+    adicionaAoTotal(preco); 
+
 }
-incrementa();
+
+function pegaPrecoItem(item){
+    var preco = item.querySelector(".preco-item");
+    return Number(preco.textContent);
+}
+
+function adicionaAoTotal(preco){
+    var total = document.querySelector("#total");
+    total.textContent = preco + Number(total.textContent);
+}
+
+function decrementa()
+{
+    input.value--;
+}
+
