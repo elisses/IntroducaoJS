7827975288ad75f7efec34678af4154fe3640652
elisses <fernandalisses@gmail.com>
2019-05-15 20:24:18 +0200

    Finalizado

--- app.js
+++ app.js
@@ -1,44 +1,77 @@
+   //Incrementa
+
+    var botoesIncrementa = document.querySelectorAll(".btn-incrementa");
+
+    for(let botao of botoesIncrementa ){
+
+        botao.addEventListener('click', incrementa);
+
+        function incrementa()
+        {
+            var item = botao.closest('.item');
+            var input = item.querySelector(".quantidade");
+            input.value++;
+            var preco = pegaPrecoItem(item);
+
+            adicionaAoTotal(preco); 
+        }
+
+    }
     
-    var botaoIncrementa = document.querySelector("#btn-incrementa");
-    botaoIncrementa.addEventListener('click', incrementa);
 
-    var botaoDecrementa = document.querySelector("#btn-decrementa");
-    botaoDecrementa.addEventListener('click', decrementa);
+//Decrementa
+    var botoesDecrementa = document.querySelectorAll(".btn-decrementa");
 
+    for(let botao of botoesDecrementa ){
+        botao.addEventListener('click', decrementa);
 
-function incrementa()
-{
-    var item = botaoIncrementa.closest('.item');
-    var input = item.querySelector(".quantidade");
-    input.value++;
-    var preco = pegaPrecoItem(item);
+        function decrementa()
+        {
+            var item = botao.closest('.item');
+            var input = item.querySelector(".quantidade");
+        
+            if(input.value > 0){
+        
+            input.value--;
+            var preco = pegaPrecoItem(item);
+            adicionaAoTotal(-preco); 
+        
+            }else{
+                console.log(input.value);
+            }
+        }
+    }
+    
+var formPedido = document.forms.pedido;
+formPedido.addEventListener('submit',function (event){
+        var contador = 0;
+        var inputs = formPedido.querySelectorAll("input.quantidade");
 
-    adicionaAoTotal(preco); 
-}
+        for(let input of inputs){
+            if(input.value > 0){
+                contador++;
+            }
+        }
+
+        if(contador === 0){
+            alert("Deve ter pelo menos uma pizza no pedido!!");
+            event.preventDefault();
+        }
+});
+
+//Pegar o preço
 
 function pegaPrecoItem(item){
     var preco = item.querySelector(".preco-item");
     return Number(preco.textContent);
 }
 
+//Adicionar o total
+
 function adicionaAoTotal(preco){
     var total = document.querySelector("#total");
     total.textContent = preco + Number(total.textContent);
 }
 
-function decrementa()
-{
-    var item = botaoIncrementa.closest('.item');
-    var input = item.querySelector(".quantidade");
 
-    if(input.value > 0){
-
-    input.value--;
-    var preco = pegaPrecoItem(item);
-    adicionaAoTotal(-preco); 
-
-    }else{
-        console.log(input.value);
-    }
-}
 
